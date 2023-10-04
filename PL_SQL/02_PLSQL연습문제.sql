be9ed95945c34438fab60280e734dc32c8355419
yangwoo <kyy77321@naver.com>
2023-10-04 20:10:13 +0900

    2023-10-04수업

--- PL_SQL/02_PLSQL연습문제.sql
+++ PL_SQL/02_PLSQL연습문제.sql
@@ -22,6 +22,24 @@ END;
 -- SELECT절 이후에 INSERT문 사용이 가능합니다.
 
 DECLARE
+    v_emp_max employees.employee_id%TYPE;
+BEGIN
+    SELECT
+        MAX(employee_id)
+    INTO
+        v_emp_max
+    FROM employees;
+    
+    INSERT INTO emps
+        (employee_id, last_name, email, hire_date, job_id)
+    VALUES(v_emp_max + 1, 'steven', 'stevenjobs', sysdate, 'CEO');
+END;
+
+SELECT * FROM emps;
+
+
+
+DECLARE
  v_emp_eid employees.employee_id%TYPE;
 BEGIN
     SELECT
